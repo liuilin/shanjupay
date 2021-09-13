a41d9ef2fd6b01a17ed44461f997f0914081eae4
liuqiang <liu1142865396@gmail.com>
2021-09-14 02:43:58 +0800

    验证码接口待验证

--- shanjupay-merchant-application/src/main/java/com/shanjupay/merchant/controller/MerchantController.java
+++ shanjupay-merchant-application/src/main/java/com/shanjupay/merchant/controller/MerchantController.java
@@ -1,25 +1,27 @@
 package com.shanjupay.merchant.controller;
 
+import cn.itcast.sailing.sms.SmsService;
 import com.shanjupay.merchant.api.MerchantService;
 import com.shanjupay.merchant.api.dto.MerchantDTO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author liuqiang
  * @version 1.0
  **/
+@Slf4j
 @RestController
 @Api(value = "商户平台应用接口", tags = "商户平台应用接口", description = "商户平台应用接口")
 public class MerchantController {
 
     @org.apache.dubbo.config.annotation.Reference
     MerchantService merchantService;
+    @org.apache.dubbo.config.annotation.Reference
+    private SmsService smsService;
 
     @ApiOperation(value = "根据id查询商户信息")
     @GetMapping("/merchants/{id}")
@@ -39,5 +41,14 @@ public class MerchantController {
     @PostMapping(value = "/hi")
     public String hi(String name) {
         return "hi," + name;
+    }
+
+    @ApiOperation("获取手机验证码")
+    @ApiImplicitParam(name = "phone", value = "手机号", required = true, dataType = "String",
+            paramType = "query")
+    @GetMapping("/sms")
+    public String getSMSCode(@RequestParam String phone) {
+        log.info("向手机号:{}发送验证码", phone);
+        return smsService.sendMsg(phone);
     }
 }
